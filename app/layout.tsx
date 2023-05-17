dddd4fb89d325659e22eeb72b0f575736f040e66
b1ondie96 <103701664+b1ondie96@users.noreply.github.com>
2023-05-17 15:52:27 +0200

    cleanup

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import { ApolloProvider } from "@apollo/client";
 import { apolloClient } from "@/utils/Apollo";
-import { CookiesProvider } from "react-cookie";
 import { AuthProvider } from "@/utils/useAuth";
 import Box from "@mui/material/Box";
 import { Typography } from "@mui/material";
@@ -23,24 +22,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <CookiesProvider>
-        <ApolloProvider client={apolloClient()}>
-          <AuthProvider>
-            <body className={inter.className}>
-              <Box
-                display={"flex"}
-                alignItems={"center"}
-                justifyContent={"center"}
-              >
-                <Link href="/">
-                  <Typography variant="h3">2048</Typography>
-                </Link>
-              </Box>
-              {children}
-            </body>
-          </AuthProvider>
-        </ApolloProvider>
-      </CookiesProvider>
+      <ApolloProvider client={apolloClient()}>
+        <AuthProvider>
+          <body className={inter.className}>
+            <Box
+              display={"flex"}
+              alignItems={"center"}
+              justifyContent={"center"}
+            >
+              <Link href="/">
+                <Typography variant="h3">2048</Typography>
+              </Link>
+            </Box>
+            {children}
+          </body>
+        </AuthProvider>
+      </ApolloProvider>
     </html>
   );
 }
